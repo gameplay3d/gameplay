bca3a74d3b77331fdaa5b091eca0780a7f70f93f
Sean Paul Taylor <setaylor@rim.com>
2011-11-14 17:09:59 -0500

    Added support for MacOSX support on gameplay and gamplay-samples suing xcode 4

---
+++ gameplay/PlatformMacOSX.h
@@ -0,0 +1,14 @@
+//
+//  PlatformMacOSX.h
+//  gameplay
+//
+//  Created by RIM User on 11-11-07.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef gameplay_PlatformMacOSX_h
+#define gameplay_PlatformMacOSX_h
+
+
+
+#endif
---
+++ sample02-spaceship/sample02-spaceship/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  sample02-spaceship
+//
+//  Created by RIM User on 11-11-12.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
