ca7e2880882e117b4e80ef35dbb4a7bfb556fa7f
Sean Paul Taylor <setaylor@rim.com>
2011-11-29 16:36:26 -0500

    Flattens/Simplifies sample resources. Fixed #46

--- gameplay-samples/sample01-longboard/src/LongboardGame.cpp
+++ gameplay-samples/sample01-longboard/src/LongboardGame.cpp
@@ -79,13 +79,13 @@ void LongboardGame::buildGround()
     _ground = Model::create(groundMesh);
 
     // Create the ground material
-    Material* groundMaterial = _ground->setMaterial("res/shaders/textured.vsh", "res/shaders/textured.fsh");
+    Material* groundMaterial = _ground->setMaterial("res/textured.vsh", "res/textured.fsh");
 
     // Set render state block
     groundMaterial->setStateBlock(_stateBlock);
 
     // Bind ground material parameters
-    Texture::Sampler* groundSampler = groundMaterial->getParameter("u_texture")->setValue("res/textures/tileable_asphalt.png", true);
+    Texture::Sampler* groundSampler = groundMaterial->getParameter("u_texture")->setValue("res/asphalt.png", true);
     groundSampler->setWrapMode(Texture::REPEAT, Texture::REPEAT);
     groundMaterial->getParameter("u_worldViewProjectionMatrix")->setValue(&_groundWorldViewProjectionMatrix);
     groundMaterial->getParameter("u_textureRepeat")->setValue(Vector2(WORLD_SIZE/2, WORLD_SIZE/2));
@@ -106,13 +106,13 @@ void LongboardGame::buildBoard()
     _board = Model::create(boardMesh);
 
     // Create the board material
-    Material* boardMaterial = _board->setMaterial("res/shaders/textured.vsh", "res/shaders/textured.fsh");
+    Material* boardMaterial = _board->setMaterial("res/textured.vsh", "res/textured.fsh");
 
     // Set render state block
     boardMaterial->setStateBlock(_stateBlock);
 
     // Bind board material parameters
-    Texture::Sampler* boardSampler = boardMaterial->getParameter("u_texture")->setValue("res/textures/longboard.png", true);
+    Texture::Sampler* boardSampler = boardMaterial->getParameter("u_texture")->setValue("res/longboard.png", true);
     boardSampler->setWrapMode(Texture::CLAMP, Texture::CLAMP);
     boardMaterial->getParameter("u_worldViewProjectionMatrix")->setValue(&_boardWorldViewProjectionMatrix);
     boardMaterial->getParameter("u_textureRepeat")->setValue(Vector2::one());
@@ -133,20 +133,20 @@ void LongboardGame::buildWheels()
     _wheels = Model::create(wheelsMesh);
 
     // Create the wheels material
-    Material* wheelsMaterial = _wheels->setMaterial("res/shaders/textured.vsh", "res/shaders/textured.fsh");
+    Material* wheelsMaterial = _wheels->setMaterial("res/textured.vsh", "res/textured.fsh");
 
     // Set render state block
     wheelsMaterial->setStateBlock(_stateBlock);
 
     // Bind wheels material parameters
-    Texture::Sampler* wheelsSampler = wheelsMaterial->getParameter("u_texture")->setValue("res/textures/wheels.png", true);
+    Texture::Sampler* wheelsSampler = wheelsMaterial->getParameter("u_texture")->setValue("res/longboard_wheels.png", true);
     wheelsSampler->setWrapMode(Texture::CLAMP, Texture::CLAMP);
     wheelsMaterial->getParameter("u_worldViewProjectionMatrix")->setValue(&_wheelsWorldViewProjectionMatrix);
     wheelsMaterial->getParameter("u_textureRepeat")->setValue(Vector2::one());
     wheelsMaterial->getParameter("u_textureTransform")->setValue(Vector2::zero());
 
     // Load audio sound
-    _wheelsSound = AudioSource::create("res/sounds/longboard2.wav");
+    _wheelsSound = AudioSource::create("res/longboard.wav");
     if (_wheelsSound)
         _wheelsSound->setLooped(true);
 
@@ -163,13 +163,13 @@ void LongboardGame::buildGradient()
     _gradient = Model::create(gradientMesh);
 
     // Create the gradient material
-    Material* gradientMaterial = _gradient->setMaterial("res/shaders/quad.vsh", "res/shaders/quad.fsh");
+    Material* gradientMaterial = _gradient->setMaterial("res/quad.vsh", "res/quad.fsh");
 
     // Set render state block
     gradientMaterial->setStateBlock(_stateBlock);
 
     // Bind material parameters
-    Texture::Sampler* gradientSampler = gradientMaterial->getParameter("u_texture")->setValue("res/textures/nice_gradient.png", false);
+    Texture::Sampler* gradientSampler = gradientMaterial->getParameter("u_texture")->setValue("res/overlay_gradient.png", false);
     gradientSampler->setWrapMode(Texture::CLAMP, Texture::CLAMP);
 
     // Release objects that are owned by mesh instances
