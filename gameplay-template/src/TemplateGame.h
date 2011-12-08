352d9b2da0f066c56905966287d733f332d0964b
Steve Grenier <sgrenier@rim.com>
2011-12-08 17:49:51 -0500

    Added a new project creation script which generates a new GamePlay project with all library dependencies and paths setup and a basic working game. Currently this script supports creation of Eclipse and VisualStudio projects.

---
+++ gameplay-template/src/TemplateGame.h
@@ -0,0 +1,56 @@
+#ifndef TEMPLATEGAME_H_
+#define TEMPLATEGAME_H_
+
+#include "gameplay.h"
+using namespace gameplay;
+
+/**
+ * Main game class.
+ */
+class TemplateGame: public Game
+{
+public:
+
+    /**
+     * Constructror.
+     */
+    TemplateGame();
+
+    /**
+     * Touch event handler.
+     */
+    void touch(int x, int y, int touchEvent);
+
+protected:
+
+    /**
+     * @see Game::initialize
+     */
+    void initialize();
+
+    /**
+     * @see Game::finalize
+     */
+    void finalize();
+
+    /**
+     * @see Game::update
+     */
+    void update(long elapsedTime);
+
+    /**
+     * @see Game::render
+     */
+    void render(long elapsedTime);
+
+private:
+
+    /**
+     * Draws the scene each frame.
+     */
+    void drawScene(Node* node, long cookie);
+
+    Scene* _scene;
+};
+
+#endif
