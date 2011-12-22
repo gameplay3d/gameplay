52da08a4f88f5ce6fc6adf2610b917615dffbf76
Darryl Gough <dgough@rim.com>
2011-12-22 17:34:18 -0500

    Revert "Initial check-in for baking COLLADA animations in the encoder."
    This reverts commit 05c8ffcad2655a5c05563421dcf8e3ed807a33c2.

--- gameplay-encoder/src/DAEUtil.cpp
+++ gameplay-encoder/src/DAEUtil.cpp
@@ -1,8 +1,6 @@
 #include "Base.h"
 #include "DAEUtil.h"
 #include "StringUtil.h"
-#include "DAEChannelTarget.h"
-#include "Vector4.h"
 
 namespace gameplay
 {
@@ -17,7 +15,7 @@ namespace gameplay
  */
 int getIndex(const domInstance_controller::domSkeleton_Array& skeletonArray, const domNodeRef& node);
 
-void getAnimationChannels(const domNodeRef& node, std::list<domChannelRef>& channels, bool recursive)
+void getAnimationChannels(const domNodeRef& node, std::list<domChannelRef>& channels)
 {
     assert(node->getId());
     std::string nodeIdSlash (node->getId());
@@ -51,20 +49,17 @@ void getAnimationChannels(const domNodeRef& node, std::list<domChannelRef>& chan
         }
     }
 
-    if (recursive)
+    // Recursively do the same for all nodes
+    daeTArray< daeSmartRef<daeElement> > children;
+    node->getChildren(children);
+    size_t childCount = children.getCount();
+    for (size_t i = 0; i < childCount; ++i)
     {
-        // Recursively do the same for all nodes
-        daeTArray< daeSmartRef<daeElement>> children;
-        node->getChildren(children);
-        size_t childCount = children.getCount();
-        for (size_t i = 0; i < childCount; ++i)
+        daeElementRef childElement = children[i];
+        if (childElement->getElementType() == COLLADA_TYPE::NODE)
         {
-            daeElementRef childElement = children[i];
-            if (childElement->getElementType() == COLLADA_TYPE::NODE)
-            {
-                domNodeRef childNode = daeSafeCast<domNode>(childElement);
-                getAnimationChannels(childNode, channels);
-            }
+            domNodeRef childNode = daeSafeCast<domNode>(childElement);
+            getAnimationChannels(childNode, channels);
         }
     }
 }
@@ -265,199 +260,6 @@ const domInstance_controller::domSkeletonRef getSkeleton(const domInstance_contr
         }
     }
     return NULL;
-}
-
-void getStartAndEndTime(const domChannelRef& channel, float* startTime, float* endTime)
-{
-    domSourceRef source = getInputSource(channel);
-    const domFloat_arrayRef floatArray = source->getFloat_array();
-    const domListOfFloats& floatList = floatArray->getValue();
-    assert(floatList.getCount() > 0);
-    *startTime = (float)floatList.get(0);
-    *endTime = (float)floatList.get(floatList.getCount() - 1);
-}
-
-void getStartAndEndTime(const std::list<domChannelRef>& channels, float* startTime, float* endTime)
-{
-    float minStart = FLT_MAX, maxEnd = 0, start = FLT_MAX, end = 0;
-    for (std::list<domChannelRef>::const_iterator i = channels.begin(); i != channels.end(); ++i)
-    {
-        getStartAndEndTime(*i, &start, &end);
-        minStart = std::min(start, minStart);
-        maxEnd = std::max(end, maxEnd);
-    }
-    assert(minStart <= maxEnd);
-    *startTime = minStart;
-    *endTime = maxEnd;
-}
-
-DAEChannelTarget* getChannelTarget(std::list<DAEChannelTarget>& targets, const char* sid)
-{
-    if (!sid)
-        return NULL;
-
-    for (std::list<DAEChannelTarget>::const_iterator i = targets.begin(); i != targets.end(); ++i)
-    {
-        
-    }
-    return NULL;
-}
-
-void getRotate(Vector4& rotate, float time)
-{
-    // if property is ANGLE
-
-}
-
-void bakeNodeTransform(domNodeRef& node)
-{
-    Matrix nodeBakedMatrix;
-
-    daeTArray<daeSmartRef<daeElement>> children;
-    node->getChildren(children);
-    size_t childCount = children.getCount();
-    for (size_t i = 0; i < childCount; ++i)
-    {
-        daeElementRef childElement = children[i];
-        switch (childElement->getElementType())
-        {
-            case COLLADA_TYPE::TRANSLATE:
-                {
-                    domTranslateRef translateNode = daeSafeCast<domTranslate>(childElement);
-                    float x = (float)translateNode->getValue().get(0);
-                    float y = (float)translateNode->getValue().get(1);
-                    float z = (float)translateNode->getValue().get(2);
-                    nodeBakedMatrix.translate(x, y, z);
-                    break;
-                }
-            case COLLADA_TYPE::ROTATE:
-                {
-                    domRotateRef rotateNode = daeSafeCast<domRotate>(childElement);
-                    Vector4 rotate;
-                    rotate.x = (float)rotateNode->getValue().get(0);
-                    rotate.y = (float)rotateNode->getValue().get(1);
-                    rotate.z = (float)rotateNode->getValue().get(2);
-                    rotate.w = MATH_DEG_TO_RAD((float)rotateNode->getValue().get(3)); // COLLADA uses degrees, gameplay uses radians
-                    if (rotate.w != 0.0f)
-                    {
-                        nodeBakedMatrix.rotate(rotate.x, rotate.y, rotate.z, rotate.w);
-                    }
-                    break;
-                }
-            case COLLADA_TYPE::SCALE:
-                {
-                    domScaleRef scaleNode = daeSafeCast<domScale>(childElement);
-                    float x = (float)scaleNode->getValue().get(0);
-                    float y = (float)scaleNode->getValue().get(1);
-                    float z = (float)scaleNode->getValue().get(2);
-                    nodeBakedMatrix.scale(x, y, z);
-                    break;
-                }
-            default:
-                break;
-        }
-    }
-    // go through the list backwards because elements will be removed
-    for (int i = childCount - 1; i >= 0; --i)
-    {
-        daeElementRef childElement = children[i];
-        switch (childElement->getElementType())
-        {
-            case COLLADA_TYPE::TRANSLATE:
-            case COLLADA_TYPE::ROTATE:
-            case COLLADA_TYPE::SCALE:
-            case COLLADA_TYPE::EXTRA:
-                daeElement::removeFromParent(childElement);
-            default:
-                break;
-        }
-    }
-
-    domMatrixRef mat = daeSafeCast<domMatrix>(node->createAndPlaceAt(0, "matrix"));
-    mat->setSid("transform");
-    domFloat4x4 floatMatrix;
-    floatMatrix.setCount(16);
-    floatMatrix.set(0, nodeBakedMatrix.m[0]);
-    floatMatrix.set(1, nodeBakedMatrix.m[4]);
-    floatMatrix.set(2, nodeBakedMatrix.m[8]);
-    floatMatrix.set(3, nodeBakedMatrix.m[12]);
-    floatMatrix.set(4, nodeBakedMatrix.m[1]);
-    floatMatrix.set(5, nodeBakedMatrix.m[5]);
-    floatMatrix.set(6, nodeBakedMatrix.m[9]);
-    floatMatrix.set(7, nodeBakedMatrix.m[13]);
-    floatMatrix.set(8, nodeBakedMatrix.m[2]);
-    floatMatrix.set(9, nodeBakedMatrix.m[6]);
-    floatMatrix.set(10, nodeBakedMatrix.m[10]);
-    floatMatrix.set(11, nodeBakedMatrix.m[14]);
-    floatMatrix.set(12, nodeBakedMatrix.m[3]);
-    floatMatrix.set(13, nodeBakedMatrix.m[7]);
-    floatMatrix.set(14, nodeBakedMatrix.m[11]);
-    floatMatrix.set(15, nodeBakedMatrix.m[15]);
-    mat->setValue(floatMatrix);
-}
-
-void getNodeTransform(const domNodeRef& node, std::list<domChannelRef>& channels, float time, Matrix& dstTransform)
-{
-    // for each transform element in node
-    // modify the transform element if one (or more?) of the channels targets it
-    // bake the transform
-
-    std::list<DAEChannelTarget> targets;
-
-    //DAEChannelTarget channelTarget(
-    for (std::list<domChannelRef>::const_iterator i = channels.begin(); i != channels.end(); ++i)
-    {
-        targets.push_back(DAEChannelTarget(*i));
-    }
-    // TODO sort targets?
-
-    daeTArray<daeSmartRef<daeElement>> children;
-    node->getChildren(children);
-    size_t childCount = children.getCount();
-    for (size_t i = 0; i < childCount; ++i)
-    {
-        daeElementRef childElement = children[i];
-        switch (childElement->getElementType())
-        {
-            case COLLADA_TYPE::TRANSLATE:
-                {
-                    domTranslateRef translateNode = daeSafeCast<domTranslate>(childElement);
-                    float x = (float)translateNode->getValue().get(0);
-                    float y = (float)translateNode->getValue().get(1);
-                    float z = (float)translateNode->getValue().get(2);
-                    dstTransform.translate(x, y, z);
-                    break;
-                }
-            case COLLADA_TYPE::ROTATE:
-                {
-                    domRotateRef rotateNode = daeSafeCast<domRotate>(childElement);
-                    //rotateNode->getSid();
-                    Vector4 rotate;
-                    rotate.x = (float)rotateNode->getValue().get(0);
-                    rotate.y = (float)rotateNode->getValue().get(1);
-                    rotate.z = (float)rotateNode->getValue().get(2);
-                    rotate.w = MATH_DEG_TO_RAD((float)rotateNode->getValue().get(3)); // COLLADA uses degrees, gameplay uses radians
-
-                    if (rotate.w != 0.0f)
-                    {
-                        dstTransform.rotate(rotate.x, rotate.y, rotate.z, rotate.w);
-                    }
-                    break;
-                }
-            case COLLADA_TYPE::SCALE:
-                {
-                    domScaleRef scaleNode = daeSafeCast<domScale>(childElement);
-                    float x = (float)scaleNode->getValue().get(0);
-                    float y = (float)scaleNode->getValue().get(1);
-                    float z = (float)scaleNode->getValue().get(2);
-                    dstTransform.scale(x, y, z);
-                    break;
-                }
-            default:
-                break;
-        }
-
-    }
 }
 
 bool equalKeyTimes(const domSource* s1, const domSource* s2)
