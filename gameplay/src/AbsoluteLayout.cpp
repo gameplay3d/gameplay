f66c8bfc90fe46063dade46a33e7b3288b89f5f6
Adam Blake <ablake@rim.com>
2012-01-24 17:07:23 -0500

    More UI layouts and controls.

---
+++ gameplay/src/AbsoluteLayout.cpp
@@ -0,0 +1,33 @@
+#include "Base.h"
+#include "AbsoluteLayout.h"
+
+namespace gameplay
+{
+    AbsoluteLayout::AbsoluteLayout()
+    {
+    }
+
+    AbsoluteLayout::AbsoluteLayout(const AbsoluteLayout& copy)
+    {
+    }
+
+    AbsoluteLayout::~AbsoluteLayout()
+    {
+    }
+
+    AbsoluteLayout* AbsoluteLayout::create()
+    {
+        AbsoluteLayout* al = new AbsoluteLayout();
+        return al;
+    }
+
+    Layout::Type AbsoluteLayout::getType()
+    {
+        return Layout::LAYOUT_ABSOLUTE;
+    }
+
+    void AbsoluteLayout::update(std::vector<Control*> controls, const Vector2& size)
+    {
+        // An AbsoluteLayout does nothing to modify the layout of Controls.
+    }
+}
