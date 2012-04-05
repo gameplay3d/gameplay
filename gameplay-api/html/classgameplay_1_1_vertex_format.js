9afbb132dfd728440ef9c91ee20906ce33453174
setaylor <setaylor@rim.com>
2012-04-05 11:35:49 -0400

    Updated doxygen api documentation.

---
+++ gameplay-api/html/classgameplay_1_1_vertex_format.js
@@ -0,0 +1,12 @@
+var classgameplay_1_1_vertex_format =
+[
+    [ "Usage", "classgameplay_1_1_vertex_format.html#a8ae70c40a5000a4e4b52cdca35923c26", null ],
+    [ "VertexFormat", "classgameplay_1_1_vertex_format.html#a3477e747689699fbfbd6bd85a4cbad11", null ],
+    [ "~VertexFormat", "classgameplay_1_1_vertex_format.html#a0781b181edd2bcb50df82a8e46c90a43", null ],
+    [ "getElement", "classgameplay_1_1_vertex_format.html#a568f735523ff8a3d636cb779f3e5ec70", null ],
+    [ "getElementCount", "classgameplay_1_1_vertex_format.html#a1a60f640e8ab4e8a951dac15e5cd582f", null ],
+    [ "getVertexSize", "classgameplay_1_1_vertex_format.html#a2c27ab28bf3ba6ce1d9309991ab77e85", null ],
+    [ "operator!=", "classgameplay_1_1_vertex_format.html#a3df43d156327ac05b071f021f898cc9e", null ],
+    [ "operator==", "classgameplay_1_1_vertex_format.html#a94f4c76295589edb2f94c62d7a7f43ff", null ],
+    [ "toString", "classgameplay_1_1_vertex_format.html#af7fae715cbac828842b4db07b6e4e6ff", null ]
+];
---
+++ gameplay-api/html/search/classes_69.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['image',['Image',['../classgameplay_1_1_image.html',1,'gameplay']]]
+];
