23f45c90f898b0cd94263864a2097d60ad6bd773
setaylor <setaylor@rim.com>
2012-04-18 13:48:31 -0400

    Updated android samples application names

--- gameplay/src/Base.h
+++ gameplay/src/Base.h
@@ -199,7 +199,6 @@ extern void printError(const char* format, ...);
     #define glClearDepth glClearDepthf
     #define OPENGL_ES
     #define USE_PVRTC
-	//#define USE_VAO
 #elif WIN32
     #define WIN32_LEAN_AND_MEAN
     #include <GL/glew.h>
