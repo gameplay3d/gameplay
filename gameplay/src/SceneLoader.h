7dd0fc15ef56f49004140778eee9a509eaee8091
Chris Culy <cculy@rim.com>
2012-04-19 13:40:44 -0400

    Added support for loading any object that can load from a Properties object using a url instead of just a file name.

--- gameplay/src/SceneLoader.h
+++ gameplay/src/SceneLoader.h
@@ -20,9 +20,13 @@ class SceneLoader
 private:
 
     /**
-     * Loads a scene file file.
+     * Loads a scene using the data from the Properties object defined at the specified URL, 
+     * where the URL is of the format "<file-path>.<extension>#<namespace-id>/<namespace-id>/.../<namespace-id>"
+     * (and "#<namespace-id>/<namespace-id>/.../<namespace-id>" is optional). 
+     * 
+     * @param url The URL pointing to the Properties object defining the scene.
      */
-    static Scene* load(const char* filePath);
+    static Scene* load(const char* url);
     
     /**
      * Helper structures and functions for SceneLoader::load(const char*).
