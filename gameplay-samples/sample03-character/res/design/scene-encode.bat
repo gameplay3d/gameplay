56c7cc3e28a236a1bf1cf398062f1542714354a9
seanpaultaylor <setaylor@rim.com>
2012-06-26 10:01:08 -0400

    Updated documentation for shader changes and characters movements -> animations

--- gameplay-samples/sample03-character/res/design/scene-encode.bat
+++ gameplay-samples/sample03-character/res/design/scene-encode.bat
@@ -2,8 +2,8 @@
 
 REM This script converts scene.dae to a gameplay binary format (.gpb) and groups all animations
 REM targeting nodes/joints under 'HIKCharacterNode_Boy_Reference' the specified id into a single 
-REM animation called 'movements'.
+REM animation called 'animations'.
 
-"../../../../gameplay-encoder/Debug/gameplay-encoder.exe" -g HIKCharacterNode_Boy_Reference movements scene.dae
+"../../../../gameplay-encoder/Debug/gameplay-encoder.exe" -g HIKCharacterNode_Boy_Reference animations scene.dae
 
 pause
