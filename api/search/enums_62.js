1d9c641f55160ac9e7a24b2435f9542c39b450a7
setaylor <setaylor@rim.com>
2012-07-25 13:49:01 -0400

    gameplay v1.4.0 api changes.

--- api/search/enums_62.js
+++ api/search/enums_62.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['blend',['Blend',['../classgameplay_1_1_render_state.html#ac4da745bdd2aef1c14407a5ac3f75285',1,'gameplay::RenderState']]]
+  ['blend',['Blend',['../classgameplay_1_1_render_state.html#ac4da745bdd2aef1c14407a5ac3f75285',1,'gameplay::RenderState']]],
+  ['buttonstate',['ButtonState',['../classgameplay_1_1_gamepad.html#a694e7bfa39b124303ee059e0635e9486',1,'gameplay::Gamepad']]]
 ];
