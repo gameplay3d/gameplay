9d71bc867fb83750a26fbf67a41a0e9a40c61521
setaylor <setaylor@rim.com>
2013-01-19 16:32:37 -0500

    Moving the forms and postprocess resources to their own folders.

--- gameplay-tests/src/PostProcessTest.cpp
+++ gameplay-tests/src/PostProcessTest.cpp
@@ -104,28 +104,28 @@ void PostProcessTest::initialize()
     // Create our compositors that all output to the default framebuffer.
     Compositor* compositor = NULL;
 
-    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess.material", "Passthrough");
+    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess/postprocess.material", "Passthrough");
     _compositors.push_back(compositor);
 
-    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess.material", "Grayscale");
+    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess/postprocess.material", "Grayscale");
     _compositors.push_back(compositor);
 
-    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess.material", "Sepia");
+    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess/postprocess.material", "Sepia");
     _compositors.push_back(compositor);
 
-    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess.material", "Pixelate");
+    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess/postprocess.material", "Pixelate");
     _compositors.push_back(compositor);
 
-    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess.material", "Sobel Edge");
+    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess/postprocess.material", "Sobel Edge");
     _compositors.push_back(compositor);
     compositor->getMaterial()->getParameter("u_width")->setValue((float)FRAMEBUFFER_WIDTH / 2.0f);
     compositor->getMaterial()->getParameter("u_height")->setValue((float)FRAMEBUFFER_HEIGHT / 2.0f);
 
-    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess.material", "Gaussian Blur");
+    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess/postprocess.material", "Gaussian Blur");
     _compositors.push_back(compositor);
     compositor->getMaterial()->getParameter("u_length")->setValue(1.0f / ((float)FRAMEBUFFER_WIDTH / 2.0f));
 
-    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess.material", "Old Film");
+    compositor = Compositor::create(_frameBuffer, NULL, "res/common/postprocess/postprocess.material", "Old Film");
     _compositors.push_back(compositor);
     compositor->getMaterial()->getParameter("u_sepiaValue")->setValue(0.8f);
     compositor->getMaterial()->getParameter("u_noiseValue")->setValue(0.4f);
