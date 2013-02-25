b493ad892917552a7303c3f3fc96b5dc6ade4c84
Scaiman <0xba13c082@gmail.com>
2013-02-25 21:16:46 +0400

    fixed #826 add check for dirty state container

--- gameplay/src/Container.cpp
+++ gameplay/src/Container.cpp
@@ -473,14 +473,15 @@ void Container::draw(SpriteBatch* spriteBatch, const Rectangle& clip, bool needs
 
     std::vector<Control*>::const_iterator it;
     Rectangle boundsUnion = Rectangle::empty();
-	struct sort_visibility
+	struct sort_visibility_container
 	{
 		static bool compare(const Control* c1, const Control* c2)
 		{
 			return c1->_visible == false;
 		}
 	};
-	std::sort(_controls.begin(), _controls.end(), sort_visibility::compare);
+	if(_dirty)
+		std::sort(_controls.begin(), _controls.end(), sort_visibility_container::compare);
 
     for (it = _controls.begin(); it < _controls.end(); it++)
     {
