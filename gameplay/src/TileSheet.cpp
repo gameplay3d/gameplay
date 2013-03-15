4ce4784e123430d1701e1b8143cd5000a2582260
Rcmaniac25 <rcmaniac25@hotmail.com>
2013-03-15 13:16:13 -0400

    Just the basics to create a TileSheet.

--- gameplay/src/TileSheet.cpp
+++ gameplay/src/TileSheet.cpp
@@ -6,17 +6,57 @@
 namespace gameplay
 {
 
+// Global list of active scenes
+static std::vector<TileSheet*> _tileSheets;
+
 TileSheet::TileSheet(const char* id)
+	: _id(id ? id : ""), _batch(NULL)
 {
-	if (id)
-    {
-        _id = id;
-    }
 }
 
 TileSheet::~TileSheet()
 {
+	SAFE_DELETE(_batch);
+
+	// Remove self from vector.
+    std::vector<TileSheet*>::iterator it = std::find(_tileSheets.begin(), _tileSheets.end(), this);
+    if (it != _tileSheets.end())
+    {
+        _tileSheets.erase(it);
+    }
 }
+
+TileSheet* TileSheet::create(const char* id, Texture* texture, unsigned int initialCapacity)
+{
+	SpriteBatch* sb = SpriteBatch::create(texture, NULL, initialCapacity);
+
+	TileSheet* tileSheet = new TileSheet(id);
+	tileSheet->_batch = sb;
+
+	_tileSheets.push_back(tileSheet);
+
+	return tileSheet;
+}
+
+TileSheet* TileSheet::getTileSheet(const char* id)
+{
+	GP_ASSERT(id);
+
+    // Search the vector for a matching ID.
+    std::vector<TileSheet*>::const_iterator it;
+    for (it = _tileSheets.begin(); it != _tileSheets.end(); ++it)
+    {
+        TileSheet* ts = *it;
+        GP_ASSERT(ts);
+        if (strcmp(id, ts->getId()) == 0)
+        {
+            return ts;
+        }
+    }
+    return NULL;
+}
+
+//TODO
 
 const char* TileSheet::getId() const
 {
--- gameplay/src/TileSheet.h
+++ gameplay/src/TileSheet.h
@@ -15,7 +15,28 @@ class TileSheet : public Ref
 
 public:
 
-	//TODO
+	/**
+	 * Creates a new TileSheet for drawing individual sprites and tiles from.
+	 * 
+	 * @param id The ID for the new TileSheet.
+	 * @param texture The texture that represents the TileSheet.
+	 * @param initialCapacity An optional initial capacity of the SpriteBatch.
+	 * 
+	 * @return A new TileSheet for drawing.
+	 * @script{create}
+	 */
+	static TileSheet* create(const char* id, Texture* texture, unsigned int initialCapacity = 0);
+
+	//TODO: create function for using a Properties (takes a url to the properties)
+
+	/**
+     * Get a named TileSheet from its ID.
+     *
+     * @param id The ID of the TileSheet to search for.
+     *
+     * @return The TileSheet with the specified ID, or NULL if one was not found.
+     */
+	static TileSheet* getTileSheet(const char* id);
 
 	/**
      * Gets the identifier for the tilesheet.
@@ -28,24 +49,15 @@ public:
 
 protected:
 
-	/**
-     * Constructor.
-     */
     TileSheet(const char* id);
 
-    /**
-     * Destructor.
-     */
     virtual ~TileSheet();
-
-	//TODO
-
-private:
 
     TileSheet(const TileSheet& copy);
     TileSheet& operator=(const TileSheet&);
 
     std::string _id;
+	SpriteBatch* _batch;
 
 	//TODO
 };
