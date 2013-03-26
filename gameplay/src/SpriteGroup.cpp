c7ebcbefa6cb2e080913b5d956c30c0c24b7138f
Rcmaniac25 <rcmaniac25@hotmail.com>
2013-03-26 02:28:21 -0400

    Implemented SpriteGroup drawing

--- gameplay/src/SpriteGroup.cpp
+++ gameplay/src/SpriteGroup.cpp
@@ -1,6 +1,7 @@
 #include "Base.h"
 #include "SpriteGroup.h"
 #include "Node.h"
+#include "Scene.h"
 
 namespace gameplay
 {
@@ -214,8 +215,172 @@ bool SpriteGroup::setSprite(unsigned int x, unsigned int y, Sprite* sprite)
 
 void SpriteGroup::draw(bool isolateDraw)
 {
-	//TODO
-	Sprite::draw(isolateDraw); //Temp
+	SpriteBatch* defBatch = _tileSheet->getSpriteBatch();
+	std::vector<SpriteBatch*> processedBatches;
+
+	Vector2 groupSize = Vector2((_width + _horzGap) * _groupWidth, (_height + _vertGap) * _groupHeight);
+
+	//Determine rotation point for entire map
+	Vector2 rotationPoint = groupSize * 0.5f;
+
+	Vector3 pos;
+	Vector2 size = Vector2(_width, _height);
+	float angle = 0.0f;
+
+	const Matrix* vpMatrix = NULL;
+	Camera* activeCamera = NULL;
+
+	//Adjust values that rely on Node
+	if (Node* node = getNode())
+	{
+		//Position
+		pos = node->getTranslationWorld();
+
+		//Rotation
+		const Quaternion& rot = node->getRotation();
+		if (rot.x != 0.0f || rot.y != 0.0f || rot.z != 0.0f) //We only want to check these values to determine if we need to calculate rotation.
+		{
+			angle = atan2f(2.0f * rot.x * rot.y + 2.0f * rot.z * rot.w, 1.0f - 2.0f * ((rot.y * rot.y) + (rot.z * rot.z)));
+		}
+
+		//Scale the size
+		size.x *= node->getScaleX();
+		size.y *= node->getScaleY();
+
+		//Get camera
+		activeCamera = node->getScene()->getActiveCamera();
+
+		if (isolateDraw && activeCamera)
+		{
+			//Adjust projection matrix if this draw is specific to this Sprite
+			vpMatrix = &(activeCamera->getViewProjectionMatrix());
+			defBatch->setProjectionMatrix(*vpMatrix);
+		}
+	}
+	else
+	{
+		pos = Vector3::zero();
+	}
+
+	//Offset position
+	pos.x += _x;
+	pos.y += _y;
+
+	//Some defaults
+	Vector2 defSize(size);
+	if ((_flip & FLIP_HORZ) == FLIP_HORZ)
+	{
+		defSize.x = -defSize.x;
+	}
+	if ((_flip & FLIP_VERT) == FLIP_VERT)
+	{
+		defSize.y = -defSize.y;
+	}
+	Rectangle defSrc = _defaultTileInUse ? _defaultTile : _frame;
+
+	std::map<unsigned int, Sprite*>::const_iterator it = _children.begin();
+	bool noChildren = it == _children.end();
+
+	//Prepare for draw loop
+	if (isolateDraw)
+	{
+		defBatch->start();
+	}
+
+	//Draw loop
+	for(unsigned int x = 0; x < _groupWidth; x++)
+	{
+		Vector3 sTempPos = pos;
+		sTempPos.x += x * (size.x + _horzGap);
+		for(unsigned int y = 0; y < _groupHeight; y++)
+		{
+			sTempPos.y += y ? (size.y + _vertGap) : 0;
+
+			Vector3 sPos = sTempPos;
+			bool drawDefault = true;
+			if(!noChildren)
+			{
+				unsigned int index = x * _groupWidth + y;
+				if(it->first == index)
+				{
+					Sprite* s = it->second;
+					noChildren = ++it == _children.end();
+
+					if(s)
+					{
+						//Custom sprite
+						drawDefault = false;
+
+						SpriteBatch* batch = s->_tileSheet->getSpriteBatch();
+
+						if(isolateDraw)
+						{
+							//Is this a new SpriteBatch?
+							std::vector<SpriteBatch*>::iterator bit = std::find(processedBatches.begin(), processedBatches.end(), batch);
+							if(bit == processedBatches.end())
+							{
+								//New SpriteBatch
+								processedBatches.push_back(batch);
+								if(vpMatrix)
+								{
+									batch->setProjectionMatrix(*vpMatrix);
+								}
+								batch->start();
+							}
+						}
+
+						//TODO: figure out how to draw multi-sized sprites using this (AKA< don't use size... but then how do we process the values? Keep a list of each one like the SpriteBatches?)
+
+						//Prepare for drawing
+						Vector2 sSize = size;
+
+						//Handle flip
+						if ((s->_flip & FLIP_HORZ) == FLIP_HORZ)
+						{
+							sPos.x += sSize.x;
+							sSize.x = -sSize.x;
+						}
+						if ((s->_flip & FLIP_VERT) == FLIP_VERT)
+						{
+							sPos.y += sSize.y;
+							sSize.y = -sSize.y;
+						}
+
+						//Draw
+						batch->draw(sPos, s->_defaultTileInUse ? s->_defaultTile : s->_frame, sSize, s->_tint, rotationPoint, angle);
+					}
+					else
+					{
+						//Transparent, skip
+						continue;
+					}
+				}
+			}
+
+			//If nothing was drawn, draw default
+			if(drawDefault)
+			{
+				if ((_flip & FLIP_HORZ) == FLIP_HORZ)
+				{
+					sPos.x -= defSize.x;
+				}
+				if ((_flip & FLIP_VERT) == FLIP_VERT)
+				{
+					sPos.y -= defSize.y;
+				}
+				defBatch->draw(sPos, defSrc, defSize, _tint, rotationPoint, angle);
+			}
+		}
+	}
+
+	if (isolateDraw)
+	{
+		defBatch->finish();
+		for(std::vector<SpriteBatch*>::iterator bit = processedBatches.begin(); bit != processedBatches.end(); bit++)
+		{
+			(*bit)->finish();
+		}
+	}
 }
 
 Sprite* SpriteGroup::clone(NodeCloneContext &context)
