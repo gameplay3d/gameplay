6bc93041d7c18d3a5279ef38b90cc8f9649eadbb
mozeal <mozeal@Jims-MacBook-Pro.local>
2013-03-31 12:28:51 +0700

    .

--- install.sh
+++ install.sh
@@ -8,9 +8,10 @@
 # be hosted separately.
 #
 
+# prefix=http://www.gameplay3d.org
 # prefix=http://www.jimmydev.com
 # prefix=http://jfinale.appoverdrive.com
-prefix=http://www.clicknectdev.com
+prefix=http://jim.clicknect.com
 
 branchname=$1
 if [[ "$branchname" == "" ]]; then
