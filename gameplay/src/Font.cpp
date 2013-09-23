a61f2701de7c74769e18f424d1288f11f96b0688
Steve Grenier <sgrenier@rim.com>
2013-09-22 22:57:07 -0400

    Fixed issue with font stretching when drawing onto a frame buffer that is a different size than the original window size.

--- gameplay/src/Font.cpp
+++ gameplay/src/Font.cpp
@@ -154,6 +154,14 @@ unsigned int Font::getSize()
 void Font::start()
 {
     GP_ASSERT(_batch);
+
+    // Update the projection matrix for our batch to match the current viewport
+    const Rectangle& vp = Game::getInstance()->getViewport();
+    Game* game = Game::getInstance();
+    Matrix projectionMatrix;
+    Matrix::createOrthographicOffCenter(vp.x, vp.width, vp.height, vp.y, 0, 1, &projectionMatrix);
+    _batch->setProjectionMatrix(projectionMatrix);
+
     _batch->start();
 }
 
