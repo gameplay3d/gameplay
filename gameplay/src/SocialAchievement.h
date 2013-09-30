gameplay3d/gameplay

commit fcff3fca77ad51ef1492e1eea0b58c7988c48cd1

- Adds initial support for Social API with Scoreloop impl for BlackBerry. - Cleans up some PREPROCESSOR macros for consistency. - Fixes a few warnings.

gameplay/src/SocialAchievement.h
@@ -1,0 +1,58 @@
+#ifndef SOCIALACHIEVEMENT_H_
+#define SOCIALACHIEVEMENT_H_
+
+namespace gameplay
+{
+
+/**
+ * Defines a class for typical social game activities running with an autenicated session.
+ */
+class SocialAchievement
+{
+
+public:
+
+    /**
+     * The name/title of the achievement.
+     */
+    std::string name;
+
+    /**
+     * The value of progress (0 to getTotal) representing the current unit value of completion.
+     */
+    unsigned int value;
+
+    /**
+     * The total value of progress needed to complete the achievement.
+     */
+    unsigned int total;
+
+    /**
+     * The percent that the achievement is complete. Equivalent to getValue() / getTotal() * 100.0f
+     */
+    float percentCompleted;
+
+    /**
+     * The date/time the achievement was achieved. (UTC string format "2013-06-27T07:40::05")
+     */
+    std::string dateTimeAchieved;
+
+    /**
+     * The provider handle for an achievement.
+     */
+    SocialAchievementHandle handle;
+
+    /**
+     * Constructor.
+     */
+    SocialAchievement();
+
+    /**
+     * Destructor.
+     */
+    ~SocialAchievement();
+};
+
+}
+
+#endif

gameplay/src/SocialController.h
@@ -1,0 +1,86 @@
+#ifndef SOCIALCONTROLLER_H_
+#define SOCIALCONTROLLER_H_
+
+#include "Properties.h"
+#include "SocialSession.h"
+#include "SocialSessionListener.h"
+
+namespace gameplay
+{
+
+/**
+ * Defines a class for controlling various back-end support for social capabilities.
+ *
+ * Back-end social providers include Friends, Leaderboards, Acheivements and Multiplayer.
+ * Social back-ends supported are Scoreloop, GameCenter and Google Play Game Services and Facebook.
+ * Configuration for these are specified in game.config:
+
+  @verbatim
+    social
+    {
+         provider = Scoreloop
+         id  = d346c484-12aa-49a2-a0a0-de2f87492d72
+         secret = aAa+DehBfyGO/CYaE3nWomgu7SIbWFczUih+Qwf3/n7u0y3nyq5Hag==
+         version = "1.0"
+         language = "en"
+         currency = "ASC"
+         leaderboard_mappings
+         {
+             // Format: leaderboardId = provider value
+             easy = 0
+             medium = 1
+             hard = 2
+         }
+    }
+
+ */
+class SocialController
+{
+    friend class Game;
+
+public:
+
+    /**
+     * Asyncrhonously authenicates a new user session.
+     *
+     * @param listener The listener
+     */
+    void authenticate(SocialSessionListener* listener);
+
+private:
+
+    /**
+     * Constructor.
+     */
+    SocialController();
+
+    /**
+     * Destructor.
+     */
+    ~SocialController();
+
+    /**
+     * Callback for when the controller is initialized.
+     */
+    void initialize();
+
+    /*
+     * Callback for when the controller is finalized.
+     */
+    void finalize();
+
+    /**
+     * Callback for when the controller receives a frame update event.
+     */
+    void update(float elapsedTime);
+
+private:
+
+    SocialSession* _session;
+
+};
+
+}
+
+#endif
+

gameplay/src/SocialSession.h
@@ -1,0 +1,90 @@
+#ifndef SOCIALSESSION_H_
+#define SOCIALSESSION_H_
+
+#include "SocialSessionListener.h"
+#include "SocialPlayer.h"
+#include "SocialAchievement.h"
+#include "SocialScore.h"
+#include "Properties.h"
+
+namespace gameplay
+{
+
+/**
+ * Defines an abstract class for typical social game activities running with an authenticated session.
+ */
+class SocialSession
+{
+    friend class SocialController;
+
+public:
+
+    enum CommunityScope
+    {
+        COMMUNITY_SCOPE_FRIENDS,
+        COMMUNITY_SCOPE_ALL,
+    };
+
+    enum TimeScope
+    {
+        TIME_SCOPE_TODAY,
+        TIME_SCOPE_WEEK,
+        TIME_SCOPE_ALL,
+    };
+
+    /**
+     * Gets the asynchronous response listener that registered for this session.
+     *
+     * @return The asynchronous response listener that registered for this session.
+     */
+    virtual SocialSessionListener* getListener() = 0;
+
+    virtual SocialPlayer* getUser() = 0;
+
+    virtual void loadFriends() = 0;
+
+    virtual void loadAchievements() = 0;
+
+    virtual void submitAchievement(const char* achievementId, unsigned int value) = 0;
+
+    /**
+     * Asynchronously request the scores for the count where the player is in the middle.
+     *
+     * @param leaderboardId The leaderboard to get populated witth the scores.
+     * @param community The community scope to filter the search list.
+     * @param time The time scope to filter teh search list.
+     * @param player The player to narrow the search around.
+     * @param count The number to scores return in the result.
+     */
+    virtual void loadScores(const char* leaderboardId, CommunityScope community, TimeScope time, const SocialPlayer& player, unsigned int count) = 0;
+
+    /**
+     * Asynchronously request the scores for a specified leaderboard for this game.
+     *
+     * @param leaderboardId The leaderboard to get populated witth the scores.
+     */
+    virtual void loadScores(const char* leaderboardId, CommunityScope community, TimeScope time, unsigned int start, unsigned int count) = 0;
+
+    virtual void submitScore(const char* leaderboardId, float value) = 0;
+
+    virtual void loadSavedData(const char* key) = 0;
+
+    virtual void submitSavedData(const char* key, std::string data) = 0;
+
+protected:
+
+    /**
+     * Contructor
+     */
+    SocialSession() { }
+
+    /**
+     * Destructor
+     */
+    virtual ~SocialSession() { }
+
+};
+
+}
+
+#endif

gameplay/src/social/ScoreloopSocialSession.h
@@ -1,0 +1,170 @@
+#if defined(__QNX__) && defined(GP_USE_SOCIAL)
+
+#ifndef SCORELOOPSOCIALSESSION_H_
+#define SCORELOOPSOCIALSESSION_H_
+
+#include "SocialSession.h"
+#include <scoreloop/sc_client.h>
+#include <scoreloop/sc_init.h>
+#include <pthread.h>
+
+namespace gameplay
+{
+
+/**
+ * Scoreloop implementation of SocialSession
+ *
+ * Note: ensure game.config has the following properties
+ *
+  @verbatim
+    social
+    {
+         provider = Scoreloop
+         id  = d346c484-12aa-49a2-a0a0-de2f87492d72
+         secret = aAa+DehBfyGO/CYaE3nWomgu7SIbWFczUih+Qwf3/n7u0y3nyq5Hag==
+         version = "1.0"
+         language = "en"
+         currency = "ASC"
+         leaderboard_mappings
+         {
+             // Format: leaderboardId =  mode <unsigned int>
+             easy = 0
+             medium = 1
+             hard = 2
+         }
+    }
+ *
+ * Note: Ensure your bar-descriptor.xml has:
+ *
+ * <action>read_device_identifying_information</action>
+ *
+ * @script{ignore}
+ */
+class ScoreloopSocialSession : public SocialSession
+{
+    friend class SocialController;
+
+public:
+
+    /**
+     * @see SocialSession::getListener
+     */
+    SocialSessionListener* getListener();
+
+    /**
+     * Initializes the session with the local client definition for this game.
+     *
+     * @param listener The listener for responses for this session
+     * @param properties The properties to initialize this session with for this game.
+     */
+    static void authenicate(SocialSessionListener* listener, Properties* properties);
+
+    /**
+     * @see SocialSession::getUser
+     */
+    SocialPlayer* getUser();
+
+    /**
+     * @see SocialSession::loadFriends
+     */
+    void loadFriends();
+
+    /**
+     * @see SocialSession::loadAchievements
+     */
+    void loadAchievements();
+
+    /**
+     * @see SocialSession::submitAchievement
+     */
+    void submitAchievement(const char* achievementId, unsigned int value);
+
+    /**
+     * @see SocialSession::loadScores
+     */
+    void loadScores(const char* leaderboardId, CommunityScope community, TimeScope time, const SocialPlayer& player, unsigned int count);
+
+    /**
+     * @see SocialSession::loadScores
+     */
+    void loadScores(const char* leaderboardId, CommunityScope community, TimeScope time, unsigned int start, unsigned int count);
+
+    /**
+     * @see SocialSession::submitScore
+     */
+    void submitScore(const char* leaderboardId, float score);
+
+    /**
+     * @see SocialSession::requestSavedData
+     */
+    void loadSavedData(const char* key);
+
+    /**
+     * @see SocialSession::submitSavedData
+     */
+    void submitSavedData(const char* key, std::string data);
+
+
+private:
+
+    /**
+     * Contructor
+     */
+    ScoreloopSocialSession();
+
+    /**
+     * Destructor
+     */
+    virtual ~ScoreloopSocialSession();
+
+    static void authenticate(SocialSessionListener* listener, Properties* properties);
+
+    static void* platformEventCallback(void* data);
+
+    static void userCallback(void* cookie, unsigned int result);
+
+    static void loadFriendsCallback(void* cookie, SC_Error_t result);
+
+    static void loadAchievementsCallback(void* cookie, SC_Error_t result);
+
+    static void submitAchievementCallback(void* cookie, SC_Error_t result);
+
+    static void loadScoresCallback(void* cookie, SC_Error_t result);
+
+    static void submitScoreCallback(void* cookie, SC_Error_t result);
+
+    static ScoreloopSocialSession* _session;
+
+    enum UserOp
+    {
+        USEROP_GET_LOCALUSER,
+        USEROP_WRITE_CONTEXT,
+        USEROP_READ_CONTEXT
+    };
+
+    SocialSessionListener* _listener;
+    Properties* _properties;
+    bool _pendingResponse;
+    SC_InitData_t _initData;
+    SC_Client_h _client;
+    SC_UserController_h _userController;
+    SC_UsersController_h _usersController;
+    SC_LocalAchievementsController_h _localAchievementsController;
+    SC_ScoresController_h _scoresController;
+    SC_ScoreController_h _scoreController;
+    pthread_cond_t _channelCond;
+    pthread_mutex_t _channelMutex;
+    const char* _key;
+    std::string _data;
+    SocialPlayer _user;
+    UserOp _userOp;
+    std::vector<SocialPlayer> _friends;
+    std::vector<SocialAchievement> _achievements;
+    std::vector<SocialScore> _scores;
+};
+
+}
+
+#endif
+
+#endif

gameplay/.cproject
@@ -24,6 +24,7 @@
 								<option id="com.qnx.qcc.option.compiler.defines.1481323494" name="Defines (-D)" superClass="com.qnx.qcc.option.compiler.defines" valueType="definedSymbols">
 									<listOptionValue builtIn="false" value="_FORTIFY_SOURCE=2"/>
 									<listOptionValue builtIn="false" value="BT_USE_NEON"/>
+									<listOptionValue builtIn="false" value="GP_USE_SOCIAL"/>
 								</option>
 								<option id="com.qnx.qcc.option.compiler.includePath.2133604142" name="Include Directories (-I)" superClass="com.qnx.qcc.option.compiler.includePath" valueType="includePath">
 									<listOptionValue builtIn="false" value="&quot;../../external-deps/lua/include&quot;"/>
@@ -87,6 +88,7 @@
 								<option id="com.qnx.qcc.option.compiler.defines.398688299" name="Defines (-D)" superClass="com.qnx.qcc.option.compiler.defines" valueType="definedSymbols">
 									<listOptionValue builtIn="false" value="_FORTIFY_SOURCE=2"/>
 									<listOptionValue builtIn="false" value="BT_USE_NEON"/>
+									<listOptionValue builtIn="false" value="GP_USE_SOCIAL"/>
 								</option>
 								<option id="com.qnx.qcc.option.compiler.includePath.1670164593" name="Include Directories (-I)" superClass="com.qnx.qcc.option.compiler.includePath" valueType="includePath">
 									<listOptionValue builtIn="false" value="&quot;../../external-deps/bullet/include&quot;"/>
@@ -145,6 +147,8 @@
 								<option id="com.qnx.qcc.option.compiler.security.1671403331" name="Enhanced Security (-fstack-protector-strong)" superClass="com.qnx.qcc.option.compiler.security" value="false" valueType="boolean"/>
 								<option id="com.qnx.qcc.option.compiler.defines.1863269886" name="Defines (-D)" superClass="com.qnx.qcc.option.compiler.defines" valueType="definedSymbols">
 									<listOptionValue builtIn="false" value="_FORTIFY_SOURCE=2"/>
+									<listOptionValue builtIn="false" value="BT_USE_NEON"/>
+									<listOptionValue builtIn="false" value="GP_USE_SOCIAL"/>
 								</option>
 								<option id="com.qnx.qcc.option.compiler.includePath.847642559" name="Include Directories (-I)" superClass="com.qnx.qcc.option.compiler.includePath" valueType="includePath">
 									<listOptionValue builtIn="false" value="&quot;../../external-deps/bullet/include&quot;"/>
@@ -215,4 +219,5 @@
 	</storageModule>
 	<storageModule moduleId="com.qnx.tools.ide.qde.core.QNXProjectProperties"/>
 	<storageModule moduleId="org.eclipse.cdt.make.core.buildtargets"/>
+	<storageModule moduleId="org.eclipse.cdt.core.LanguageSettingsProviders"/>
 </cproject>

gameplay/gameplay.vcxproj
@@ -318,6 +318,12 @@
     <ClCompile Include="src\ScriptController.cpp" />
     <ClCompile Include="src\ScriptTarget.cpp" />
     <ClCompile Include="src\Slider.cpp" />
+    <ClCompile Include="src\SocialController.cpp" />
+    <ClCompile Include="src\social\ScoreloopSocialAchievement.cpp" />
+    <ClCompile Include="src\social\ScoreloopSocialLeaderboard.cpp" />
+    <ClCompile Include="src\social\ScoreloopSocialPlayer.cpp" />
+    <ClCompile Include="src\social\ScoreloopSocialScore.cpp" />
+    <ClCompile Include="src\social\ScoreloopSocialSession.cpp" />
     <ClCompile Include="src\SpriteBatch.cpp" />
     <ClCompile Include="src\Technique.cpp" />
     <ClCompile Include="src\Terrain.cpp" />
@@ -599,6 +605,18 @@
     <ClInclude Include="src\ScriptController.h" />
     <ClInclude Include="src\ScriptTarget.h" />
     <ClInclude Include="src\Slider.h" />
+    <ClInclude Include="src\SocialAchievement.h" />
+    <ClInclude Include="src\SocialController.h" />
+    <ClInclude Include="src\SocialLeaderboard.h" />
+    <ClInclude Include="src\SocialPlayer.h" />
+    <ClInclude Include="src\SocialScore.h" />
+    <ClInclude Include="src\SocialSession.h" />
+    <ClInclude Include="src\SocialSessionListener.h" />
+    <ClInclude Include="src\social\ScoreloopSocialAchievement.h" />
+    <ClInclude Include="src\social\ScoreloopSocialLeaderboard.h" />
+    <ClInclude Include="src\social\ScoreloopSocialPlayer.h" />
+    <ClInclude Include="src\social\ScoreloopSocialScore.h" />
+    <ClInclude Include="src\social\ScoreloopSocialSession.h" />
     <ClInclude Include="src\SpriteBatch.h" />
     <ClInclude Include="src\Stream.h" />
     <ClInclude Include="src\Technique.h" />
@@ -914,7 +932,7 @@
       </PrecompiledHeader>
       <WarningLevel>Level3</WarningLevel>
       <Optimization>Disabled</Optimization>
-      <PreprocessorDefinitions>USE_BLACKBERRY_GAMEPAD;_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;GP_USE_SOCIAL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>$(ProjectDir)src;..\external-deps\lua\include;..\external-deps\bullet\include;..\external-deps\openal\include\AL;..\external-deps\alut\include\AL;..\external-deps\oggvorbis\include;..\external-deps\glew\include;..\external-deps\libpng\include;..\external-deps\zlib\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <RuntimeTypeInfo>
       </RuntimeTypeInfo>
@@ -949,7 +967,7 @@
       </PrecompiledHeader>
       <WarningLevel>Level3</WarningLevel>
       <Optimization>Disabled</Optimization>
-      <PreprocessorDefinitions>_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;GAMEPLAY_MEM_LEAK_DETECTION;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;GP_USE_MEM_LEAK_DETECTION;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>$(ProjectDir)src;..\external-deps\lua\include;..\external-deps\bullet\include;..\external-deps\openal\include\AL;..\external-deps\alut\include\AL;..\external-deps\oggvorbis\include;..\external-deps\glew\include;..\external-deps\libpng\include;..\external-deps\zlib\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <RuntimeTypeInfo>true</RuntimeTypeInfo>
       <RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>
@@ -973,7 +991,7 @@
       </PrecompiledHeader>
       <WarningLevel>Level3</WarningLevel>
       <Optimization>Disabled</Optimization>
-      <PreprocessorDefinitions>_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;GAMEPLAY_MEM_LEAK_DETECTION;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;GP_USE_MEM_LEAK_DETECTION;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>$(ProjectDir)src;..\external-deps\lua\include;..\external-deps\bullet\include;..\external-deps\openal\include\AL;..\external-deps\alut\include\AL;..\external-deps\oggvorbis\include;..\external-deps\glew\include;..\external-deps\libpng\include;..\external-deps\zlib\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <RuntimeTypeInfo>true</RuntimeTypeInfo>
       <RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>
@@ -993,7 +1011,7 @@
       </PrecompiledHeader>
       <WarningLevel>Level3</WarningLevel>
       <Optimization>Disabled</Optimization>
-      <PreprocessorDefinitions>__BB10__;_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;GAMEPLAY_MEM_LEAK_DETECTION;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;GP_USE_MEM_LEAK_DETECTION;GP_USE_SOCIAL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>$(ProjectDir)src;..\external-deps\lua\include;..\external-deps\bullet\include;..\external-deps\openal\include\AL;..\external-deps\alut\include\AL;..\external-deps\oggvorbis\include;..\external-deps\glew\include;..\external-deps\libpng\include;..\external-deps\zlib\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <RuntimeTypeInfo>true</RuntimeTypeInfo>
       <RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>
@@ -1014,7 +1032,7 @@
       </PrecompiledHeader>
       <WarningLevel>Level3</WarningLevel>
       <Optimization>Disabled</Optimization>
-      <PreprocessorDefinitions>_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;GAMEPLAY_MEM_LEAK_DETECTION;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>_ITERATOR_DEBUG_LEVEL=0;WIN32;_DEBUG;_LIB;GP_USE_MEM_LEAK_DETECTION;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>$(ProjectDir)src;..\external-deps\lua\include;..\external-deps\bullet\include;..\external-deps\openal\include\AL;..\external-deps\alut\include\AL;..\external-deps\oggvorbis\include;..\external-deps\glew\include;..\external-deps\libpng\include;..\external-deps\zlib\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <RuntimeTypeInfo>true</RuntimeTypeInfo>
       <RuntimeLibrary>MultiThreadedDLL</RuntimeLibrary>
@@ -1076,7 +1094,7 @@
       <Optimization>MaxSpeed</Optimization>
       <FunctionLevelLinking>true</FunctionLevelLinking>
       <IntrinsicFunctions>true</IntrinsicFunctions>
-      <PreprocessorDefinitions>WIN32;NDEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>WIN32;NDEBUG;_LIB;GP_USE_SOCIAL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>$(ProjectDir)src;..\external-deps\lua\include;..\external-deps\bullet\include;..\external-deps\openal\include\AL;..\external-deps\alut\include\AL;..\external-deps\oggvorbis\include;..\external-deps\glew\include;..\external-deps\libpng\include;..\external-deps\zlib\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
       <AdditionalOptions>-mfpu=neon %(AdditionalOptions)</AdditionalOptions>
     </ClCompile>
@@ -1095,7 +1113,7 @@
       <Optimization>MaxSpeed</Optimization>
       <FunctionLevelLinking>true</FunctionLevelLinking>
       <IntrinsicFunctions>true</IntrinsicFunctions>
-      <PreprocessorDefinitions>WIN32;NDEBUG;_LIB;%(PreprocessorDefinitions)</PreprocessorDefinitions>
+      <PreprocessorDefinitions>WIN32;NDEBUG;_LIB;GP_USE_SOCIAL;%(PreprocessorDefinitions)</PreprocessorDefinitions>
       <AdditionalIncludeDirectories>$(ProjectDir)src;..\external-deps\lua\include;..\external-deps\bullet\include;..\external-deps\openal\include\AL;..\external-deps\alut\include\AL;..\external-deps\oggvorbis\include;..\external-deps\glew\include;..\external-deps\libpng\include;..\external-deps\zlib\include;%(AdditionalIncludeDirectories)</AdditionalIncludeDirectories>
     </ClCompile>
     <Link>

gameplay/gameplay.vcxproj.filters
@@ -13,6 +13,9 @@
     <Filter Include="src\lua">
       <UniqueIdentifier>{21cf31c6-9c10-44cb-a864-d46a0e7bfe5e}</UniqueIdentifier>
     </Filter>
+    <Filter Include="src\social">
+      <UniqueIdentifier>{7bd5ee6c-c0cc-4ae3-b7a9-443fb391b944}</UniqueIdentifier>
+    </Filter>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="src\Animation.cpp">
@@ -861,6 +864,24 @@
     <ClCompile Include="src\lua\lua_RenderStateStencilOperation.cpp">
       <Filter>src\lua</Filter>
     </ClCompile>
+    <ClCompile Include="src\SocialController.cpp">
+      <Filter>src</Filter>
+    </ClCompile>
+    <ClCompile Include="src\social\ScoreloopSocialAchievement.cpp">
+      <Filter>src\social</Filter>
+    </ClCompile>
+    <ClCompile Include="src\social\ScoreloopSocialLeaderboard.cpp">
+      <Filter>src\social</Filter>
+    </ClCompile>
+    <ClCompile Include="src\social\ScoreloopSocialPlayer.cpp">
+      <Filter>src\social</Filter>
+    </ClCompile>
+    <ClCompile Include="src\social\ScoreloopSocialScore.cpp">
+      <Filter>src\social</Filter>
+    </ClCompile>
+    <ClCompile Include="src\social\ScoreloopSocialSession.cpp">
+      <Filter>src\social</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="src\Animation.h">
@@ -1708,6 +1729,42 @@
     </ClInclude>
     <ClInclude Include="src\lua\lua_RenderStateStencilOperation.h">
       <Filter>src\lua</Filter>
+    </ClInclude>
+    <ClInclude Include="src\SocialController.h">
+      <Filter>src</Filter>
+    </ClInclude>
+    <ClInclude Include="src\SocialSession.h">
+      <Filter>src</Filter>
+    </ClInclude>
+    <ClInclude Include="src\SocialAchievement.h">
+      <Filter>src</Filter>
+    </ClInclude>
+    <ClInclude Include="src\SocialLeaderboard.h">
+      <Filter>src</Filter>
+    </ClInclude>
+    <ClInclude Include="src\SocialPlayer.h">
+      <Filter>src</Filter>
+    </ClInclude>
+    <ClInclude Include="src\SocialSessionListener.h">
+      <Filter>src</Filter>
+    </ClInclude>
+    <ClInclude Include="src\SocialScore.h">
+      <Filter>src</Filter>
+    </ClInclude>
+    <ClInclude Include="src\social\ScoreloopSocialAchievement.h">
+      <Filter>src\social</Filter>
+    </ClInclude>
+    <ClInclude Include="src\social\ScoreloopSocialLeaderboard.h">
+      <Filter>src\social</Filter>
+    </ClInclude>
+    <ClInclude Include="src\social\ScoreloopSocialPlayer.h">
+      <Filter>src\social</Filter>
+    </ClInclude>
+    <ClInclude Include="src\social\ScoreloopSocialScore.h">
+      <Filter>src\social</Filter>
+    </ClInclude>
+    <ClInclude Include="src\social\ScoreloopSocialSession.h">
+      <Filter>src\social</Filter>
     </ClInclude>
   </ItemGroup>
   <ItemGroup>

commit 96cccac08c032d74fd5342f5c08e4e74698c31d5

Fix some typos introduced in fcff3fc

gameplay/src/SocialAchievement.h
@@ -5,7 +5,7 @@ namespace gameplay
 {
 
 /**
- * Defines a class for typical social game activities running with an autenicated session.
+ * Defines a class for typical social game activities running with an authenticated session.
  */
 class SocialAchievement
 {

gameplay/src/SocialController.h
@@ -11,7 +11,7 @@ namespace gameplay
 /**
  * Defines a class for controlling various back-end support for social capabilities.
  *
- * Back-end social providers include Friends, Leaderboards, Acheivements and Multiplayer.
+ * Back-end social providers include Friends, Leaderboards, Achievements and Multiplayer.
  * Social back-ends supported are Scoreloop, GameCenter and Google Play Game Services and Facebook.
  * Configuration for these are specified in game.config:
 
@@ -41,7 +41,7 @@ class SocialController
 public:
 
     /**
-     * Asyncrhonously authenicates a new user session.
+     * Asyncrhonously authenticates a new user session.
      *
      * @param listener The listener
      */

gameplay/src/SocialSession.h
@@ -50,7 +50,7 @@ public:
     /**
      * Asynchronously request the scores for the count where the player is in the middle.
      *
-     * @param leaderboardId The leaderboard to get populated witth the scores.
+     * @param leaderboardId The leaderboard to get populated with the scores.
      * @param community The community scope to filter the search list.
      * @param time The time scope to filter teh search list.
      * @param player The player to narrow the search around.
@@ -61,7 +61,7 @@ public:
     /**
      * Asynchronously request the scores for a specified leaderboard for this game.
      *
-     * @param leaderboardId The leaderboard to get populated witth the scores.
+     * @param leaderboardId The leaderboard to get populated with the scores.
      */
     virtual void loadScores(const char* leaderboardId, CommunityScope community, TimeScope time, unsigned int start, unsigned int count) = 0;
 

gameplay/src/social/ScoreloopSocialSession.cpp
@@ -59,7 +59,7 @@ void* ScoreloopSocialSession::platformEventCallback(void* data)
     return 0;
 }
 
-void ScoreloopSocialSession::authenicate(SocialSessionListener* listener, Properties* properties)
+void ScoreloopSocialSession::authenticate(SocialSessionListener* listener, Properties* properties)
 {
     if (_session)
         return;

gameplay/src/social/ScoreloopSocialSession.h
@@ -57,7 +57,7 @@ public:
      * @param listener The listener for responses for this session
      * @param properties The properties to initialize this session with for this game.
      */
-    static void authenicate(SocialSessionListener* listener, Properties* properties);
+    static void authenticate(SocialSessionListener* listener, Properties* properties);
 
     /**
      * @see SocialSession::getUser

samples/browser/src/InputSample.cpp
@@ -93,7 +93,7 @@ void InputSample::update(float elapsedTime)
     if (hasAccelerometer())
     {
         Vector3 accelRaw, gyroRaw;
-        getRawSensorValues(&accelRaw.x, &accelRaw.y, &accelRaw.z, &gyroRaw.x, &gyroRaw.y, &gyroRaw.z);
+        getSensorValues(&accelRaw.x, &accelRaw.y, &accelRaw.z, &gyroRaw.x, &gyroRaw.y, &gyroRaw.z);
 
         // Adjust for landscape mode
         float temp = accelRaw.x;