0a2f8f85978440df90f976a7f5aca373eb5300fa
seanpaultaylor <setaylor@blackberry.com>
2013-10-01 12:18:22 -0400

    Added some missing files.

---
+++ gameplay/src/SocialChallenge.cpp
@@ -0,0 +1,16 @@
+#include "Base.h"
+#include "SocialChallenge.h"
+
+namespace gameplay
+{
+
+SocialChallenge::SocialChallenge()
+    : state(INVALID), score(0.0f)
+{
+}
+
+SocialChallenge::~SocialChallenge()
+{
+}
+
+}
