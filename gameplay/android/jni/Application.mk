91c3b13822cfeea5b9705a2cf248219bccaefb42
setaylor <setaylor@blackberry.com>
2014-11-20 00:08:58 -0500

    Added x86 support for Android

--- gameplay/android/jni/Application.mk
+++ gameplay/android/jni/Application.mk
@@ -2,6 +2,6 @@ NDK_TOOLCHAIN_VERSION := 4.8
 APP_CPPFLAGS += -std=c++11
 APP_STL      := gnustl_static
 APP_MODULES  := libgameplay
-APP_ABI      := armeabi-v7a
+APP_ABI      := armeabi-v7a x86
 APP_PLATFORM := android-16
 
