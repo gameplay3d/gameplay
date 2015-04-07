28b5f525a5cd548a2ae7d11b079b9e20a2e66b95
Andrew Karpushin <reven86@gmail.com>
2015-04-07 21:29:49 +0400

    fixed counting hidden controls when calculating total scrollable width and height

--- gameplay/src/Container.cpp
+++ gameplay/src/Container.cpp
@@ -1087,6 +1087,9 @@ void Container::updateScroll()
     {
         Control* control = _controls[i];
 
+        if (!control->isVisible())
+            continue;
+
         const Rectangle& bounds = control->getBounds();
         const Theme::Margin& margin = control->getMargin();
 
