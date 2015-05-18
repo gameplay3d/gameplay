9da6a410d6df0a42b418f39f609293cb48f7e976
Jon Watte <jwatte@gmail.com>
2015-05-18 13:58:57 -0700

    Recognize .otf as .ttf files (the library supports them.)

--- tools/encoder/src/EncoderArguments.h
+++ tools/encoder/src/EncoderArguments.h
@@ -21,6 +21,7 @@ public:
         FILEFORMAT_FBX,
         FILEFORMAT_TMX,
         FILEFORMAT_TTF,
+        FILEFORMAT_OTF,
         FILEFORMAT_GPB,
         FILEFORMAT_PNG,
         FILEFORMAT_RAW
