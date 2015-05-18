c229ca079d5bddff72dc57323bd2b0df0c4bd64f
Jon Watte <jwatte@gmail.com>
2015-05-18 14:12:44 -0700

    Use the Ref::Hold stack object to guarantee clean-up.

--- gameplay/src/Control.cpp
+++ gameplay/src/Control.cpp
@@ -1055,7 +1055,7 @@ void Control::notifyListeners(Control::Listener::EventType eventType)
     // This method runs untrusted code by notifying listeners of events.
     // If the user calls exit() or otherwise releases this control, we
     // need to keep it alive until the method returns.
-    addRef();
+    Ref::Hold ref(this);
 
     controlEvent(eventType);
 
@@ -1074,8 +1074,6 @@ void Control::notifyListeners(Control::Listener::EventType eventType)
     }
 
     fireScriptEvent<void>(GP_GET_SCRIPT_EVENT(Control, controlEvent), dynamic_cast<void*>(this), eventType);
-
-    release();
 }
 
 void Control::controlEvent(Control::Listener::EventType evt)
