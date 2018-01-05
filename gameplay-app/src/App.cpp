8d64a462a94ff7a9e3645d953aa437d7555eaa60
seanpaultaylor <uber.setaylor@gmail.com>
2018-01-05 18:30:55 -0500

    Updates to app for testing D3D12 triangle

--- gameplay-app/src/App.cpp
+++ gameplay-app/src/App.cpp
@@ -9,12 +9,12 @@ App app;
 App::App() :
 	_vertShader(nullptr),
 	_fragShader(nullptr),
-	_descriptorSet(nullptr),
 	_uniformBuffer(nullptr),
 	_vertexBuffer(nullptr),
 	_indexBuffer(nullptr),
 	_texture(nullptr),
 	_sampler(nullptr),
+	_descriptorSet(nullptr),
 	_renderPipeline(nullptr),
 	_renderPass(nullptr)
 {
@@ -43,15 +43,12 @@ void App::onInitialize()
 											Format::FORMAT_R32G32B32_FLOAT, 0, 1, VertexLayout::toStride(attributes[0].format));
 	VertexLayout vertexLayout(attributes.data(), attributes.size());
 
-
-	//_uniformBuffer = graphics->createUniformBuffer(16 * sizeof(float), true, nullptr);
-	
 	// Create the vertex buffer
 	std::vector<float> vertices = 
 	{
          0.00f, 0.25f, 0.0f,  1.0f,    1.0f, 0.0f, 0.0f,
         -0.25f,  -0.25f, 0.0f, 1.0f,   0.0f, 1.0f, 0.0f,
-        -0.25f, -0.25f, 0.0f, 1.0f,   0.0f, 0.0f, 1.0f
+        -0.25f, -0.25f, 0.0f, 1.0f,    0.0f, 0.0f, 1.0f
     };
 
 
@@ -60,52 +57,10 @@ void App::onInitialize()
 	_vertexBuffer = graphics->createVertexBuffer(vertexDataSize, vertexStride, true, nullptr);
 	memcpy(_vertexBuffer->getHostMemory(), vertices.data(), vertexDataSize);
 
-	/* Create the index buffer
-	std::vector<uint16_t> indices = 
-	{
-        0, 1, 2,
-        0, 2, 3
-    };
-
-	size_t indexDataSize = sizeof(uint16_t) * indices.size();
-	_indexBuffer = graphics->createIndexBuffer(indexDataSize, sizeof(uint32_t), true, indices.data());
-	*/
-
-	/* Read an image, texture then upload
-	std::shared_ptr<Image> image = Image::create("res/images/logo_white.png");
-	_texture = graphics->createTexture2d(image->getWidth(), image->getHeight(), 1, 
-										 Format::FORMAT_R8G8B8A8_UNORM, 
-										 Texture::USAGE_SAMPLED_IMAGE, 
-									     Texture::SAMPLE_COUNT_1X, true, image->getData());
-
-	_sampler = graphics->createSampler(Sampler::FILTER_LINEAR, Sampler::FILTER_LINEAR, Sampler::FILTER_LINEAR,
-									   Sampler::ADDRESS_MODE_WRAP, Sampler::ADDRESS_MODE_WRAP, Sampler::ADDRESS_MODE_WRAP,
-									   Sampler::BORDER_COLOR_BLACK_OPAQUE, false, Sampler::COMPARE_FUNC_NEVER,
-									   false, 1.0f, 0.0f, 0.0f, 0.0f);
-									   */
 
 	// Acquired the initial render pass
 	_renderPass = graphics->acquireNextSwapchainImage();
 
-	/* Create the descriptor set
-	std::vector<DescriptorSet::Descriptor> descriptors(3);
-	descriptors[0].binding = 0;
-	descriptors[0].type = DescriptorSet::Descriptor::TYPE_UNIFORM;
-	descriptors[0].count = 1;
-	descriptors[0].shaderStages = DescriptorSet::Descriptor::SHADER_STAGE_VERT;
-	descriptors[0].uniforms[0] = _uniformBuffer;
-	descriptors[1].binding = 0;
-	descriptors[1].type = DescriptorSet::Descriptor::TYPE_TEXTURE;
-	descriptors[1].count = 1;
-	descriptors[1].shaderStages = DescriptorSet::Descriptor::SHADER_STAGE_FRAG;
-	descriptors[1].textures[0] = _texture;
-	descriptors[2].binding = 0;
-	descriptors[2].type = DescriptorSet::Descriptor::TYPE_SAMPLER;
-	descriptors[2].count = 1;	
-	descriptors[2].shaderStages = DescriptorSet::Descriptor::SHADER_STAGE_FRAG;
-	descriptors[2].samplers[0] = _sampler;
-	_descriptorSet = graphics->createDescriptorSet(descriptors.data(), descriptors.size());
-	*/
 
 	// Create the render pipeline
 	RasterizerState rasterizerState;
@@ -134,15 +89,6 @@ void App::onUpdate(float elapsedTime)
 
 	_renderPass = graphics->acquireNextSwapchainImage();
 
-	/*
-	Matrix worldViewProjection = Matrix::identity();
-	worldViewProjection.m[0] = cos(elapsedTime);
-	worldViewProjection.m[1] = sin(elapsedTime);
-	worldViewProjection.m[4] = -sin(elapsedTime);
-	worldViewProjection.m[5] = cos(elapsedTime);
-	memcpy(_uniformBuffer->getHostMemory(), worldViewProjection.m, 16 * sizeof(float));
-	*/
-
 	std::shared_ptr<CommandBuffer> commandBuffer = graphics->beginCommands();
 	graphics->cmdTransitionImage(commandBuffer, _renderPass->getColorAttachment(0), Texture::USAGE_PRESENT, Texture::USAGE_COLOR_ATTACHMENT);
 	graphics->cmdSetViewport(commandBuffer, 0, 0, graphics->getWidth(), graphics->getHeight(), 0.0f, 1.0f);
@@ -151,8 +97,6 @@ void App::onUpdate(float elapsedTime)
 	graphics->cmdClearColor(commandBuffer, 0.0f, 0.0f, 0.0f, 1.0f, 0);
 	graphics->cmdBindRenderPipeline(commandBuffer, _renderPipeline);
 	graphics->cmdBindVertexBuffer(commandBuffer, _vertexBuffer);
-	//graphics->cmdBindIndexBuffer(commandBuffer, _indexBuffer);
-	//graphics->cmdBindDescriptorSet(commandBuffer, _renderPipeline, _descriptorSet);
 	graphics->cmdDraw(commandBuffer, 3, 0);
 	graphics->cmdEndRenderPass(commandBuffer);
 	graphics->cmdTransitionImage(commandBuffer, _renderPass->getColorAttachment(0), Texture::USAGE_COLOR_ATTACHMENT, Texture::USAGE_PRESENT);
--- gameplay-app/src/App.h
+++ gameplay-app/src/App.h
@@ -44,12 +44,12 @@ public:
 
 	std::shared_ptr<Shader> _vertShader;
 	std::shared_ptr<Shader> _fragShader;
-	std::shared_ptr<DescriptorSet> _descriptorSet;
 	std::shared_ptr<Buffer> _uniformBuffer;
 	std::shared_ptr<Buffer> _vertexBuffer;
 	std::shared_ptr<Buffer> _indexBuffer;
 	std::shared_ptr<Texture> _texture;
 	std::shared_ptr<Sampler> _sampler;
+	std::shared_ptr<DescriptorSet> _descriptorSet;
 	std::shared_ptr<RenderPipeline> _renderPipeline;
 	std::shared_ptr<RenderPass> _renderPass;
 };
